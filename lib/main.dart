bf70906de00a5522126abafec2494315e78b9a5f
Ramiro CC <ramconcastillo@gmail.com>
2023-06-21 19:48:20 -0400

    se actualizo el texto centrado

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Presiona el boton:',
+              'Boton presionado:',
             ),
             Text(
               '$_counter',
