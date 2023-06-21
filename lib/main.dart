da7ed0faa017422f57394a7a6846983980267c08
Ramiro CC <ramconcastillo@gmail.com>
2023-06-21 19:50:19 -0400

    se cambio el titulo a git y github

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Rama appBar'),
+      home: const MyHomePage(title: 'Curso git github'),
     );
   }
 }
